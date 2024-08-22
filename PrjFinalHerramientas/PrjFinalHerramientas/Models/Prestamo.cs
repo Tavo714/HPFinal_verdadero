7bc33192fb00b10b7c543db38e133bd0c319a5d3
Gustavo Valera <gvaleramendoza@gmail.com>
2024-08-22 18:05:01 -0500

    first commit

---
+++ PrjFinalHerramientas/PrjFinalHerramientas/Models/Prestamo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrjFinalHerramientas.Models;
+
+public partial class Prestamo
+{
+    public int PrestamoId { get; set; }
+
+    public int UsuarioId { get; set; }
+
+    public int LibroId { get; set; }
+
+    public DateOnly FechaPrestamo { get; set; }
+
+    public DateOnly? FechaDevolucion { get; set; }
+
+    public int TiempoPrestamo { get; set; }
+
+    public string Estado { get; set; } = null!;
+
+    public virtual Libro Libro { get; set; } = null!;
+
+    public virtual Usuario Usuario { get; set; } = null!;
+}
